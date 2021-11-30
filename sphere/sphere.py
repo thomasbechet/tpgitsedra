7e90dbd845592e044f6da6b354b3729505818b6a
Nathan Quiblier <nathan.quiblier@inria.fr>
2021-11-30 16:50:14 +0100

    use math.pi in surface

--- sphere/sphere.py
+++ sphere/sphere.py
@@ -16,7 +16,7 @@ class Sphere(object):
     def surface(self):
         # *** STEP 3 ***
         # uncomment the following line
-        return 4.0 * 3.1416 * self.radius ** 2
+        return 4.0 * math.pi * self.radius ** 2
         pass
 
     def volume(self):
