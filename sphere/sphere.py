7b3fbe12bb1fc218c52ac44af174ec3c712cdf6a
Nathan Quiblier <nathan.quiblier@inria.fr>
2021-11-30 17:06:13 +0100

    fix buggy diameter computatiuon≈

--- sphere/sphere.py
+++ sphere/sphere.py
@@ -28,7 +28,7 @@ class Sphere(object):
     def diameter(self):
         # *** STEP 5 ***
         # uncomment the following line
-        return self.radius
+        return self.radius*2
         pass
 
     def dump(self, filename):
